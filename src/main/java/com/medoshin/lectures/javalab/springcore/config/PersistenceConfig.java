8dd8e713e73fb7a334635e8b0e1c0e4802d0ea29
Kirill <kirill_nn@inbox.ru>
2020-05-23 12:23:15 +0300

    +++

--- src/main/java/com/medoshin/lectures/javalab/springcore/config/PersistenceConfig.java
+++ src/main/java/com/medoshin/lectures/javalab/springcore/config/PersistenceConfig.java
@@ -1,7 +1,6 @@
 package com.medoshin.lectures.javalab.springcore.config;
 
 import org.hibernate.cfg.Environment;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
--- src/main/java/com/medoshin/lectures/javalab/springcore/dao/UzerDAO.java
+++
@@ -1,9 +0,0 @@
-package com.medoshin.lectures.javalab.springcore.dao;
-
-import com.medoshin.lectures.javalab.springcore.entity.Uzer;
-import org.springframework.transaction.annotation.Transactional;
-
-public interface UzerDAO {
-    @Transactional
-    Uzer getUserById(Integer id);
-}
